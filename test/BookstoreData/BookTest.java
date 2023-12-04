5d7bad718ac2cf278e5790ac14591ba1ee23cdf7
mateo <mgjini21@epoka.edu.al>
2023-12-05 00:03:43 +0100

    Book Test Added

---
+++ test/BookstoreData/BookTest.java
@@ -0,0 +1,101 @@
+package BookstoreData;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+    private Book book;
+
+    @BeforeEach
+    void setUp() {
+        book = new Book("1234567890123", "Test Book", "Description", 29.99f, "Test Author", true, 10);
+        book.addGenres(Book.Genre.FANTASY, Book.Genre.MYSTRERY);
+    }
+
+    @Test
+    void testGetIsbn13() {
+        assertEquals("1234567890123", book.getIsbn13());
+    }
+
+    @Test
+    void testGetTitle() {
+        assertEquals("Test Book", book.getTitle());
+    }
+
+    @Test
+    void testGetAuthor() {
+        assertEquals("Test Author", book.getAuthor());
+    }
+
+    @Test
+    void testGetGenre() {
+        assertEquals("FANTASY, MYSTRERY, ", book.getGenre());
+    }
+
+    @Test
+    void testGetStock() {
+        assertEquals("10", book.getStock());
+    }
+
+    @Test
+    void testGetPaperBack() {
+        assertEquals("true", book.getPaperBack());
+    }
+
+    @Test
+    void testGetPrice() {
+        assertEquals("29.99", book.getPrice());
+    }
+
+    @Test
+    void testSetIsbn13() {
+        book.setIsbn13("9876543210987");
+        assertEquals("9876543210987", book.getIsbn13());
+    }
+
+    @Test
+    void testSetTitle() {
+        book.setTitle("New Test Book");
+        assertEquals("New Test Book", book.getTitle());
+    }
+
+    @Test
+    void testSetAuthor() {
+        book.setAuthor("New Test Author");
+        assertEquals("New Test Author", book.getAuthor());
+    }
+
+    @Test
+    void testSetGenres() {
+        List<Book.Genre> newGenres = Arrays.asList(Book.Genre.ACTION, Book.Genre.HISTORICAL);
+        book.setGenres(new ArrayList<>(newGenres));
+
+        String expectedGenres = "ACTION, HISTORICAL, ";
+        String actualGenres = book.getGenre();
+
+        assertEquals(expectedGenres, actualGenres);
+    }
+
+    @Test
+    void testSetStock() {
+        book.setStock(15);
+        assertEquals("15", book.getStock());
+    }
+
+    @Test
+    void testSetPaperBack() {
+        book.setPaperBack(false);
+        assertEquals("false", book.getPaperBack());
+    }
+
+    @Test
+    void testSetPrice() {
+        book.setPrice(39.99f);
+        assertEquals("39.99", book.getPrice());
+    }
+}
