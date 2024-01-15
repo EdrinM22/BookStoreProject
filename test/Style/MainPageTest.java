979fa84e43dd3bca9ad8a5d6b8ffa786ea7b0f92
mateo <mgjini21@epoka.edu.al>
2024-01-15 21:32:45 +0100

    Added tests to improve test coverage

--- test/Style/MainPageTest.java
+++ test/Style/MainPageTest.java
@@ -5,13 +5,21 @@ import Staff.Gender;
 import Staff.Librarian;
 import Staff.Worker;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.base.NodeMatchers;
+import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.matcher.control.TextInputControlMatchers;
 
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.control.LabeledMatchers.hasText;
@@ -20,34 +28,40 @@ import static org.testfx.matcher.control.LabeledMatchers.hasText;
 class MainPageTest extends ApplicationTest {
 
     private MainPage mainPage;
-    private Worker worker;
-    @Override
+    @Start
     public void start(Stage stage) {
-        Librarian tempLibrarian = new Librarian("John Doe", "123-456-7890", "john@example.com", "01-01-1990",
+        Librarian librarian = new Librarian("John Doe", "123-456-7890", "john@example.com", "01-01-1990",
                 Gender.MALE, 5000.0f, "password", Worker.ACCESSLEVEL.LIBRARIAN, true);
-        mainPage = new MainPage(stage, tempLibrarian);
+        mainPage = new MainPage(stage, librarian);
         stage.setScene(new Scene(mainPage.getRoot(), 800, 600));
         stage.show();
-        stage.toFront();
     }
 
     @Test
-    void testPurchaseBookPane() {
-        Book book = new Book("1234567890123", "Test Book", "Description", 19.99f, "Author", true, 10);
-        book.setIsbn13("9876543210987"); // Initialize Book
-        GridPane purchaseBookPane = mainPage.getPurchaseBookPane(book);
-
-
-        mainPage.getRoot().setCenter(purchaseBookPane);
-
-
-        clickOn("#NrBooks").write("5");
-        press(javafx.scene.input.KeyCode.ENTER);
-
-        Label totalLabel = lookup("#TotalLabel").queryAs(Label.class);
-
-        verifyThat(totalLabel, hasText("Total: " + 99.95f));
+    public void shouldContainLogoutButtonWithCorrectText() {
+        verifyThat("#Logout", LabeledMatchers.hasText("Log Out"));
     }
 
+
+
+    @Test
+    public void shouldContainSearchBar() {
+        verifyThat("#Search", TextInputControlMatchers.hasText(""));
+    }
+
+    @Test
+    public void shouldEnableAddBookButtonForAdmin() {
+        verifyThat("#AddBook", NodeMatchers.isEnabled());
+    }
+
+    @Test
+    public void shouldDisableAddBookButtonForLibrarian() {
+        verifyThat("#AddBook", NodeMatchers.isDisabled());
+    }
+
+    @Test
+    public void shouldSearchForBookByTitle() {
+        clickOn("#Search").write("as").press(KeyCode.ENTER);
+    }
 
 }
